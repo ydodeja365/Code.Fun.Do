6db392678e70b5f8f6d4e0a22516f5a4290a32bf
Yash Dodeja <ydodeja365@gmail.com>
2018-03-18 14:06:41 +0530

    First

---
+++ Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour {
+    public static float speed=0.3f;//Setting speed of movement
+    // Use this for initialization
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.LookAt(Camera.main.transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, Camera.main.transform.position, step);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Has collided!");
+        if(other.CompareTag("Player"))
+        {
+            //Decrease player's health and destroy the robo
+            endTrigger();
+            Destroy(this.gameObject);
+
+        }
+        if(other.CompareTag("bullet"))
+        {
+            //Debug.Log("Shot!");
+            Destroy(other.gameObject);
+            //Increase player's score and destroy the robo
+            Game.score += (long)(Game.currentlevel * speed * 20);
+            endTrigger();
+            Destroy(this.gameObject);
+        }
+    
+    }
+    void endTrigger()
+    {
+        Game.robosHit -= 1;
+        if (Game.robosHit != 0)
+            FindObjectOfType<Game>().SpawnNewRobo();
+        else
+            SceneManager.LoadScene("Level Up");
+    }
+}
---
+++ Assets/webcam.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class webcam : MonoBehaviour {
+    public GameObject plane;    //Reference to the plane that renders camera feed
+    Quaternion newRotation;     //Storing new rotation in every frame
+
+    void Start () {
+        if(Application.isMobilePlatform)//If platform is mobile platform
+        {
+            // Mobile specific code needed to render correctly in landscape mode`
+            GameObject cameraParent = new GameObject("camParent");
+            cameraParent.transform.position = this.transform.position;
+            this.transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right,90);
+        }
+        //Enabling gyro readings for device:
+        Input.gyro.enabled = true;
+        //Rendering webcam feed on the plane
+        WebCamTexture webCamTexture = new WebCamTexture();
+        plane.GetComponent<MeshRenderer>().material.mainTexture = webCamTexture;
+        //Playing the feed
+        webCamTexture.Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Changing camera rotation based on gyro values
+        newRotation =new Quaternion(Input.gyro.attitude.x,Input.gyro.attitude.y, -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        this.transform.localRotation =newRotation;
+	}
+}
