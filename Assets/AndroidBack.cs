10b7f1a428f85837de993e0100d461507aace265
Yash Dodeja <ydodeja365@gmail.com>
2018-03-21 22:40:19 +0530

    APP DONE!

---
+++ Assets/AndroidBack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidBack : MonoBehaviour {
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void FixedUpdate()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.GetKey(KeyCode.Escape))
+                {
+                    Application.Quit();
+                }
+            }
+        }
+}
